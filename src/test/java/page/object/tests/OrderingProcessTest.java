f2b28a97e5d078ea92d2cce9de10e79991e05975
Aleks-2001 <aleks.200175@gmail.com>
2024-10-20 16:59:42 +0300

    Project without readme

---
+++ src/test/java/page/object/tests/OrderingProcessTest.java
@@ -0,0 +1,40 @@
+package page.object.tests;
+import org.junit.Assert;
+import org.junit.Test;
+import page.object.MainPage;
+import page.object.OrderPage;
+
+public class OrderingProcessTest extends BaseUITest{
+
+    @Test
+    public void firstValuesOrderingProcessTest() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        mainPage.cookieButtonConfirmClick();
+        mainPage.OrderButtonUpperClick();
+        OrderPage orderPage = new OrderPage(driver);
+        orderPage.inputValuesInOrderFormOne("Алексей", "Петров", "Комсомольская, дом 34",  "+79215672121");
+        orderPage.nextButtonClick();
+        orderPage.inputValuesInOrderFormTwo("black","заплачу чаевые");
+        orderPage.ConfirmOrderButtonClick();
+        orderPage.confirmOrderButtonYesClick();
+        boolean isDisplayedOrderSuccessful = orderPage.isDisplayedOrderSuccessful();
+        Assert.assertTrue(isDisplayedOrderSuccessful);
+    }
+    @Test
+    public void secondValuesOrderingProcessTest() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.openMainPage();
+        mainPage.cookieButtonConfirmClick();
+        mainPage.OrderButtonLowerClick();
+        OrderPage orderPage = new OrderPage(driver);
+        orderPage.inputValuesInOrderFormOne("Иоган Себастьян", "Бах", "Красная Площадь дом 1",  "49215672121");
+        orderPage.nextButtonClick();
+        orderPage.inputValuesInOrderFormTwo("gray","без комментариев");
+        orderPage.ConfirmOrderButtonClick();
+        orderPage.confirmOrderButtonYesClick();
+        boolean isDisplayedOrderSuccessful = orderPage.isDisplayedOrderSuccessful();
+        Assert.assertTrue(isDisplayedOrderSuccessful);
+    }
+
+}
